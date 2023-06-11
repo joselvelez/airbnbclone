a2a3dc066acab64843f4ff61c5af4d8d9453013c
Jose Velez <josevelez@JLVMBP.attlocal.net>
2023-06-11 12:03:00 -0500

    show listings functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,9 @@ export default async function RootLayout({
           <RentModal />
           <NavBar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
       </body>
     </html>
   )
